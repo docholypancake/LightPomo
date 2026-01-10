ce85c519d92014152c7389cf5bbb76e13261c0eb
Dr. Pancake <96749215+docholypancake@users.noreply.github.com>
2026-01-10 18:36:00 +0200

    Initial Commit

---
+++ LightPomo/LightPomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightPomoApp.swift
+//  LightPomo
+//
+//  Created by Oleh on 10.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LightPomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
